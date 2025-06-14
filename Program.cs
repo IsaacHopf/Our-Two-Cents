218a1306b6ee4fa184b04957d8fffc203f211294
Isaac Hopf <isaachopf@outlook.com>
2025-06-13 21:14:48 -0400

    Setup MainLayout and NavMenu.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using Photino.Blazor;
+
+namespace BudgetApp;
+
+public class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+        appBuilder.Services.AddLogging();
+        appBuilder.Services.AddMudServices();
+
+        appBuilder.RootComponents.Add<App>("#app");
+
+        var app = appBuilder.Build();
+
+        app.MainWindow
+            .SetSize(1400, 800)
+            .SetDevToolsEnabled(true)
+            .SetLogVerbosity(0)
+            //.SetIconFile("favicon.ico")
+            .SetTitle("Our Two Cents");
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+
+        app.Run();
+    }
+}
