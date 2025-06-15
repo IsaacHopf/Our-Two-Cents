e4a46bac2efb7232e1af28852e926fbfed89432f
Isaac Hopf <isaachopf@outlook.com>
2025-06-15 16:05:02 -0400

    Designed Budget Page.

--- Services/DateTimeExtensions.cs
+++ Services/DateTimeExtensions.cs
@@ -6,10 +6,11 @@ public static class DateTimeExtensions
 {
     public static string ToMonthName(this DateTime dateTime) =>
         dateTime.ToString("MMMM");
+    
     public static string ToMonthName(this int month) =>
         CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month);
     
-    public static string ToMonthAbbrev(this DateTime dateTime) =>
+    public static string ToMonthAbbreviation(this DateTime dateTime) =>
         dateTime.ToString("MMM");
     
     public static string ToMonthAbbreviation(this int month) =>
