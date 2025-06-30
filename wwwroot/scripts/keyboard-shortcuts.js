e99fb44af1bc567129a12cee48ee3522fd9b8118
Isaac Hopf <isaachopf@outlook.com>
2025-06-29 20:08:58 -0400

    Renamed css class.

--- wwwroot/scripts/keyboard-shortcuts.js
+++ wwwroot/scripts/keyboard-shortcuts.js
@@ -1,7 +1,7 @@
 ﻿// Used for toggling edit in TransactionsGrid.
 function executeCallbackOnEditMudDataGridOutsideClick(instance, callback) {
     document.addEventListener("click", e => {
-        if (e.target.closest(".mud-datagrid-edit-onclick")) {
+        if (e.target.closest(".mud-data-grid-edit-onclick")) {
             return;
         } else {
             instance.invokeMethodAsync(callback);
@@ -11,7 +11,7 @@ function executeCallbackOnEditMudDataGridOutsideClick(instance, callback) {
 
 // Used for adding new records in TransactionsGrid.
 function executeCallbackOnEditMudDataGridEnter(instance, callback) {
-    var elements = document.getElementsByClassName("mud-datagrid-edit-onclick");
+    var elements = document.getElementsByClassName("mud-data-grid-edit-onclick");
     for (var i = 0; i < elements.length; i++) {
         elements[i].addEventListener("keydown", e => {
             if (e.key === "Enter") {
