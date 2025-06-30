c804d79d5a05a827507fb18bbc328af90f73e8f7
Isaac Hopf <isaachopf@outlook.com>
2025-06-29 20:42:57 -0400

    Fixed FixedBudget Discriminator.

--- Services/Entities/FixedBudget.cs
+++ Services/Entities/FixedBudget.cs
@@ -3,5 +3,6 @@
 public class FixedBudget() : Budget(1, 1)
 {
     public new const string Name = "January 1";
-    public new const string Discriminator = "FixedBudget";
+    /// <remarks>Must be property or else it will be set to "Budget".</remarks>
+    public new string Discriminator { get; } = "FixedBudget";
 }
