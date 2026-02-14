18b9690e2ce723a0efa71f35d3ac217689f4fe70
Isaac Hopf <isaachopf@outlook.com>
2026-02-14 17:30:58 -0500

    Removed reminders.

--- Services/Entities/Reminder.cs
+++
@@ -1,9 +0,0 @@
-namespace BudgetApp.Services.Entities;
-
-public class Reminders
-{
-    /// <remarks>Must be property or else Cosmos won't work.</remarks>
-    public string Id { get; } = "Reminders";
-    public string Discriminator { get; } = "Reminders";
-    public List<string> Items { get; init; } = [];
-}
